bd4d3fb8a05551be7827de8348b0a71cb8022ece
ramgangula999@gmail.com <ec2-user@ip-172-31-8-223.us-east-2.compute.internal>
2026-03-06 23:29:52 +0000

    reverting Revert "updated"
    This reverts commit 71bfabb9963382a4750635e6c91b28b2ec21276f.

--- deploy.java
+++ deploy.java
@@ -1,9 +1,6 @@
 this file contains the deployment logic
 <<<<<<< HEAD
 hello upadating again
-<<<<<<< HEAD
 this kine will be added
 =======
 >>>>>>> parent of 0439a61 (updated)
-=======
->>>>>>> parent of cda084a (upadated)
