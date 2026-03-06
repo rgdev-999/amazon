71bfabb9963382a4750635e6c91b28b2ec21276f
ramgangula999@gmail.com <ec2-user@ip-172-31-8-223.us-east-2.compute.internal>
2026-03-06 23:26:17 +0000

    updated

--- deploy.java
+++ deploy.java
@@ -1,6 +1,9 @@
 this file contains the deployment logic
 <<<<<<< HEAD
 hello upadating again
+<<<<<<< HEAD
 this kine will be added
 =======
 >>>>>>> parent of 0439a61 (updated)
+=======
+>>>>>>> parent of cda084a (upadated)
